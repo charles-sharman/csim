c57b51e8a3e4d9ab6a610bca53ffcc14c171e866
Charles Sharman <css@harbona>
2022-06-01 15:14:54 -0600

    Corrected schematic path

--- __init__.py
+++ __init__.py
@@ -481,10 +481,11 @@ def print_desrev(title_slide=0, content_slide=2, specs_per_slide=7):
     for schematic in schematics:
         slide = prs.slides.add_slide(prs.slide_layouts[content_slide])
         slide.shapes.title.text = 'Schematics: %s' % os.path.splitext(schematic)[0]
-        im = Image.open(os.path.join(sdir, schematic))
+        name = os.path.join(sdir, schematic)
+        im = Image.open(name)
         left, top, width, height = _im_scale(slide.shapes[1], np.array(im.size)*(emus_per_inch / schematic_dpi))
         im.close()
-        slide.shapes.add_picture(schematic, left, top, width, height)
+        slide.shapes.add_picture(name, left, top, width, height)
 
     # Plots
     lines = _strip_file(os.path.join(os.path.abspath(config['project']), 'plots'))
